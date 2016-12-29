ca366f70aa5c295fa30d16853c044e35e536e8ef
Adam Gruber <adamg@aweber.com>
2016-12-29 11:34:14 -0500

    cleanup

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const alexa = require('alexa-app');
 const getMenuSchema = require('./schemas/getMenu');
 const getMenuIntentHandler = require('./handlers/getMenuIntent');
@@ -8,14 +10,14 @@ module.change_code = 1;
 // Define an alexa-app
 const app = new alexa.app('lunchlady');
 
-app.launch(function(req, res) {
-  res.say('Hello. I\'m the lunch lady.');
+app.launch((req, res) => {
+  res.say('Hello, I\'m the lunch lady. You can ask me what\'s for lunch and I will tell you the menu.');
 });
 
 app.intent('GetMenuIntent', getMenuSchema, getMenuIntentHandler);
 
-app.error = function(exception, request, response) {
-  response.say('Sorry, I\'m having trouble getting the menu.');
+app.error = (exception, request, response) => {
+  response.say('Sorry, I\'m having trouble getting the lunch menu.');
 };
 
 module.exports = app;
